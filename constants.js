68031ceb9f3d2fc976d73e1682076cc5aa8137b0
Russell Sasamori <russell.sasamori@gmail.com>
2021-11-30 14:35:04 -0500

    Added board[y][x] array.

--- constants.js
+++ constants.js
@@ -7,3 +7,4 @@ const divisions_x = 8;
 const divisions_y = 4;
 const cell_length_x = outer_box_x / divisions_x;
 const cell_length_y = outer_box_y / divisions_y;
+
--- draw_grid.js
+++ draw_grid.js
@@ -1,11 +1,25 @@
 console.log("Enter draw_grid.js"  + version);
 const canvas = document.querySelector('canvas')
 var ctx = canvas.getContext("2d");
-ctx.fillStyle = "#FF8080";
-ctx.fillRect(0, 0, 150, 75);
+ctx.fillStyle = "#FF8000";
+// ctx.fillRect(0, 0, 150, 75);
 
-ctx.moveTo(0, 0);
-ctx.lineTo(200, 100);
+// initialize array
+for(let i = 0; i < divisions_y; i++) {
+    for(let j = 0; j < divisions_x; j++) {
+        board[i][j] = 0;
+    }
+}
+// draw array content
+for(let i = 0; i < divisions_y; i++) {
+    let y = i * cell_length_y;
+    for(let j = 0; j < divisions_x; j++) {
+        let x = j * cell_length_x;
+        ctx.fillRect(x, y, cell_length_x * 0.75, cell_length_y * 0.75);
+    }
+}
+// ctx.moveTo(0, 0);
+// ctx.lineTo(200, 100);
 
 for(let i = 0; i < divisions_x; i++) {
     let x = i * cell_length_x;
